d75115a202d34e7fd7051edeb7c6bef5595d4755
Connorbelez <connor.belez@gmail.com>
2024-02-18 12:26:54 -0500

    linux build

--- lua/vbattle/deps/sock/build/build.sh
+++ lua/vbattle/deps/sock/build/build.sh
@@ -1,17 +1,17 @@
 [ "$P" ] || exit 1
 cat *.patch | patch -Np0
 
-mkdir -p "$(dirname ../../sock/socket/$SD)"
+mkdir -p "$(dirname ../build/bin/linux64/clib/socket/$SD)"
 ${X}gcc -c -O2 $C $files -I. -I../lua-headers
-${X}gcc *.o -shared -o ../../sock//socket/$SD $L
-rm -f      ../../sock/$P/$SA
-${X}ar rcs ../../sock/$P/$SA *.o
+${X}gcc *.o -shared -o ../build/bin/linux64/clib/socket/$SD $L
+rm -f      ../build/bin/linux64/clib/socket/$SA
+${X}ar rcs ../build/bin/linux64/$SA *.o
 rm *.o
 
-mkdir -p "$(dirname ../../sock/mime/$MD)"
+mkdir -p "$(dirname ../build/bin/linux64/clib/mime/$MD)"
 ${X}gcc -c -O2 $C src/mime.c -I. -I../lua-headers
-${X}gcc mime.o -shared -o ../../sock/mime/$MD $L
-rm -f      ../../sock/$P/$MA
-${X}ar rcs ../../sock/$P/$MA mime.o
+${X}gcc mime.o -shared -o ../build/bin/linux64/clib/mime/$MD $L
+rm -f      ../build/bin/linux64/$MA
+${X}ar rcs ../build/bin/linux64/$MA mime.o
 
 rm *.o
